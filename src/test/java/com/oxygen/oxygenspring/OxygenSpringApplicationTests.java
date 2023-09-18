7cf1ccc27c157b230336037692cbd0012c3afdb0
Anna-Jin <anna.jin.6848@gmail.com>
2023-09-18 18:51:35 +0900

    테스트 삭제

--- src/test/java/com/oxygen/oxygenspring/OxygenSpringApplicationTests.java
+++ src/test/java/com/oxygen/oxygenspring/OxygenSpringApplicationTests.java
@@ -1,12 +1,9 @@
 package com.oxygen.oxygenspring;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
+//@SpringBootTest
 class OxygenSpringApplicationTests {
 
-    @Test
+    //    @Test
     void contextLoads() {
     }
 
